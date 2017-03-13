01445deb589c9764da6076199eea57b968b79ff1
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-03-13 20:06:14 +0530

    refactor app

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const port = process.env.PORT || 8080
 const app = express()
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// Require our routes into the application.
 require('./routes')(app)
+
 app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the blog api of the century.'
+  message: 'Welcome to the blog api of the century'
 }))
 
-app.listen(8080)
+app.listen(port)
